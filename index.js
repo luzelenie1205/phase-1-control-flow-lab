19cd3a3e222a60c93aff570ffe4a9a4c2dd5e64a
luzelenie <luzelenie1205@gmail.com>
2021-07-26 23:46:03 -0400

    commit tree 4ce3fbb124a7f776eab5e7a43f1f7e65e0af79e8

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+
+  if ( 400 >= someValue){
+    return "This one is on me!"
+  }
+  else if  (2000 <= someValue && 2500 >= someValue){
+return "I will gladly take your thirty bucks."
+  }
+  else {
+    return  "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":return "Thank you so much."
+    break;
+    case "not as generous" : return "Thank you."
+    break;
+    default: return 'Bye.'
+  }
+
 }
